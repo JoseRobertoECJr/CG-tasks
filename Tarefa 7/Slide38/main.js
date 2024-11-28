0a8633d7cc5feef52eb535a908e7ab1c99a58209
JoseRobertoECJr <joserobertoecjr@gmail.com>
2024-11-27 21:51:20 -0300

    terminando o slide38

--- Tarefa 7/Slide38/main.js
+++ Tarefa 7/Slide38/main.js
@@ -8,12 +8,14 @@ import { vec2 } from '../vec.js';
 export function main() {
 
     const P = lineFunc(new vec2Col([20, 300], red), new vec2Col([280, 200], red));
+    const Q = lineFunc(new vec2Col([20, 20], blue), new vec2Col([80, 334], blue));
 
     let Img = new imageRGB();
 
     Img.fill(white);
     
     Img.render2d([
-        [P, points]
+        [P, points],
+        [Q, points]
     ]);
 }
