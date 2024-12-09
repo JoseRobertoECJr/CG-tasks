404371147017fe154eab11ffeb48fa8c2338f6e2
JoseRobertoECJr <joserobertoecjr@gmail.com>
2024-12-08 23:34:22 -0300

    tarefa 8 no final

--- Tarefa 8/TestRender/main/app.js
+++
@@ -1,3 +0,0 @@
-import { main } from './main.js';
-
-window.onload = main;
--- Tarefa 8/TestRender/triangle.js
+++
@@ -1,5 +0,0 @@
-export function triangle(vec2ColorA, vec2ColorB, vec2ColorC) {
-    this.a = vec2ColorA;
-    this.b = vec2ColorB;
-    this.c = vec2ColorC;
-}
